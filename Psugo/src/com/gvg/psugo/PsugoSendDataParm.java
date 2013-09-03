05c042b01fb081e28f21e5d3d05e5fca910fbfab
Owner <Owner@192.168.1.194>
2013-09-03 09:27:04 -0400

    09/03

---
+++ Psugo/src/com/gvg/psugo/PsugoSendDataParm.java
@@ -0,0 +1,23 @@
+package com.gvg.psugo;
+
+import android.content.Context;
+
+public class PsugoSendDataParm {
+
+	public Context theContext;
+	public String dataToSend;
+	
+	public PsugoSendDataParm() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+	public PsugoSendDataParm(Context theContext, String dataToSend) {
+		super();
+		this.theContext = theContext;
+		this.dataToSend = dataToSend;
+	}
+	
+	
+
+}
